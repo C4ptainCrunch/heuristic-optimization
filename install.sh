e8c6e4bd9f73c3e20620700998bd5c3bf24bbb82
Nikita Marchant <nikita.marchant@gmail.com>
2017-05-16 17:37:40 +0200

    .

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@ sudo dnf install gcc make git
 
 run=$(date "+%d/%m/%y-%Hh")
 
-echo "Starting run $run" >> $run.log
+echo "Starting run $run" > $run.log
 
 touch result.zip
 
-tmpfile=$( mktemp -t transfer$run );
+tmpfile=$( mktemp -t transferXXX );
 curl --upload-file "result.zip" "http://partou.se/upload/jemhW/run" >> $tmpfile;
 cat $tmpfile >> $run.log
 rm -f $tmpfile;
