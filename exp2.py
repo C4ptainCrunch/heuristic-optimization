d97456fbdd38c27c6e267bc02fc13e3e18739566
Nikita Marchant <nikita.marchant@gmail.com>
2017-05-17 10:36:11 +0200

    .

---
+++ exp2.py
@@ -0,0 +1,17 @@
+import multiprocessing
+import json
+from datetime import datetime
+
+from exp1 import star_run, instances50
+
+if __name__ == '__main__':
+    jobs = [(instance, "ils", 70 * 100) for instance in instances50[:5]] * 25
+
+    print(len(jobs))
+
+    with open("exp1-%s.log" % datetime.now().strftime("%d_%H:%M"), "w") as fd:
+        with multiprocessing.Pool(None) as pool:
+            for res in pool.imap(star_run, jobs):
+                fd.write(json.dumps(res))
+                fd.write("\n")
+                fd.flush()
