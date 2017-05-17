2bf80355fd1dab6b84a9f87d5f11571c34d0fb8e
Nikita Marchant <nikita.marchant@gmail.com>
2017-05-17 19:17:03 +0200

    now genetic

--- exp2.py
+++ exp2.py
@@ -5,11 +5,11 @@ from datetime import datetime
 from exp1 import star_run, instances50
 
 if __name__ == '__main__':
-    jobs = [(instance, "ils", 70 * 10) for instance in sorted(instances50)[:5]] * 25
+    jobs = [(instance, "genetic", 70 * 10) for instance in sorted(instances50)[:5]] * 25
 
     print(len(jobs))
 
-    with open("exp2-%s.log" % datetime.now().strftime("%d_%H:%M"), "w") as fd:
+    with open("exp2-genetic-%s.log" % datetime.now().strftime("%d_%H:%M"), "w") as fd:
         with multiprocessing.Pool(None) as pool:
             for res in pool.imap(star_run, jobs):
                 fd.write(json.dumps(res))
